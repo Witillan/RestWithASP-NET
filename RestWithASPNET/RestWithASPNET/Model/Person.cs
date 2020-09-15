3301caef3e19cad8f33bc4c4efafddb9750821b9
Developer02 <developer02@alphasoftware.br>
2020-09-15 14:16:43 -0400

    Implementing all principal verbs

---
+++ RestWithASPNET/RestWithASPNET/Model/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASPNET.Model
+{
+    public class Person
+    {
+        public long Id { get; }
+        public string FirstName { get; }
+        public string LastName { get; }
+        public string Address { get; }
+        public string Gender { get; }
+    }
+}
